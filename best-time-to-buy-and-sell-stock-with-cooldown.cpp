83a413bdfb62a367c7b89cfb64b7ff498ceb778c
darion-yaphet <darion-yaphet@gmail.com>
2025-04-24 23:38:00 +0800

    25.04.24

--- best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,9 +5,31 @@
 
 using namespace std;
 
+// https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-cooldown/description
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
+        // 如果数组为空，直接返回 0
+        if (prices.empty()) {
+            return 0;
+        }
 
+        int hold = INT32_MIN, sold = 0, cooldown = 0;
+
+        for (int price : prices) {
+            int prevHold = hold, prevSold = sold, prevCooldown = cooldown;
+
+            // 更新持有股票的状态
+            hold = max(prevHold, prevCooldown - price);
+
+            // 更新卖出股票的状态
+            sold = prevHold + price;
+
+            // 更新冷冻期的状态
+            cooldown = max(prevCooldown, prevSold);
+        }
+
+        // 最大利润是最后一天的 sold 或 cooldown 中的最大值
+        return max(sold, cooldown);
     }
 };
--- trapping-rain-water.cpp
+++ trapping-rain-water.cpp
@@ -41,4 +41,51 @@ public:
         }
         return sum;
     }
+
+    /**
+     * 使用两个指针 left 和 right 分别从数组的两端向中间移动。
+     *
+     * 维护两个变量 maxLeft 和 maxRight，分别表示从左到右和从右到左的最大高度。
+     *
+     * 每次比较 height[left] 和 height[right]：
+     * 如果 height[left] <= height[right]，说明左侧的柱子较低，决定当前水位的是 maxLeft。
+     * 如果 height[left] < maxLeft，则可以接水，水量为 maxLeft - height[left]。
+     * 否则更新 maxLeft，移动左指针 left++。
+     *
+     * 如果 height[left] > height[right]，说明右侧的柱子较低，决定当前水位的是 maxRight。
+     * 如果 height[right] < maxRight，则可以接水，水量为 maxRight - height[right]。
+     * 否则更新 maxRight，移动右指针 right--。
+     */
+    int trap2(vector<int> &height) {
+        // 如果数组为空，直接返回 0
+        if (height.empty()) {
+            return 0;
+        }
+
+        int left = 0, right = height.size() - 1; // 左右指针
+        int maxLeft = 0, maxRight = 0; // 左右最大高度
+        int totalWater = 0; // 接雨水的总量
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                // 左侧柱子较低，决定水位的是 maxLeft
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left]; // 更新左侧最大高度
+                } else {
+                    totalWater += maxLeft - height[left]; // 累计接水
+                }
+                ++left; // 移动左指针
+            } else {
+                // 右侧柱子较低，决定水位的是 maxRight
+                if (height[right] >= maxRight) {
+                    maxRight = height[right]; // 更新右侧最大高度
+                } else {
+                    totalWater += maxRight - height[right]; // 累计接水
+                }
+                --right; // 移动右指针
+            }
+        }
+
+        return totalWater;
+    }
 };
